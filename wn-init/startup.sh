dc21efb2877b8342a00a99d2939ab1e90f33ea4d
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-06-14 11:33:54 +0200

    Add more diagnostic messages

--- wn-init/startup.sh
+++ wn-init/startup.sh
@@ -5,7 +5,9 @@ mkdir -p /docker-init/htcondor/downloads
 cp /opt/htcondor/downloads/* /docker-init/htcondor/downloads/
 
 cp /opt/htcondor/*.sh /docker-init/htcondor/
+echo "HTCOndor init files in /docker-init/htcondor"
 
 if [ -e "${INIT_USER}" ]; then
   chown -R "${INIT_USER}" /docker-init/htcondor
+  echo "Change ownership of /docker-init/htcondor to ${INIT_USER}"
 fi
