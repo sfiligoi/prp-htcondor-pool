a22d37d9501d688ed0d6deffffc94848fcebffc7
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-06-14 11:16:14 +0200

    Add option to chown to a different user

--- wn-init/startup.sh
+++ wn-init/startup.sh
@@ -5,3 +5,7 @@ mkdir -p /docker-init/htcondor/downloads
 cp /opt/htcondor/downloads/* /docker-init/htcondor/downloads/
 
 cp /opt/htcondor/*.sh /docker-init/htcondor/
+
+if [ -e "${INIT_USER}" ]; then
+  chown -R "${INIT_USER}" /docker-init/htcondor
+fi
