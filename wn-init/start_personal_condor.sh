0729d0681af7160fde7debb9caa978bb13a3c40f
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-06-14 12:45:00 +0200

    Clean non-root startup

--- wn-init/start_personal_condor.sh
+++ wn-init/start_personal_condor.sh
@@ -5,26 +5,6 @@
 # 
 #
 
-myuname=`id -un`
-
-if [ "${HTCONDOR_USER}" == "" ]; then
-  if [ "${HTCONDOR_USER}" == "root" ]; then
-    export HTCONDOR_USER=condor
-  else
-    export HTCONDOR_USER=$myuname
-  fi
-fi
-echo "HTCondor daemons will run as user ${HTCONDOR_USER}"
-
-id "${HTCONDOR_USER}"
-if [ $? -ne 0 ]; then
-  useradd "${HTCONDOR_USER}"
-  if [ $? -ne 0 ]; then
-     echo "ERROR: Cannot crearte user ${HTCONDOR_USER}. Aborting."
-     exit 1
-  fi
-fi
-
 (cd /docker-init/htcondor/downloads/ && ./unpack.sh)
 rc=$?
 if [ $rc -ne 0 ]; then
@@ -39,23 +19,50 @@ if [ $rc -ne 0 ]; then
   exit 1
 fi
 
-(cd /opt/htcondor && ./condor_install --make-personal-condor --owner=${HTCONDOR_USER} --local-dir=/var/lib/htcondor )
-if [ $? -ne 0 ]; then
-   echo "ERROR: condor_install failed. Aborting."
-   exit 1
+myuname=`id -un`
+
+if [ "${HTCONDOR_USER}" == "root" ]; then
+
+  if [ "${HTCONDOR_USER}" == "" ]; then
+    export HTCONDOR_USER=condor
+  fi
+
+  echo "HTCondor daemons will run as user ${HTCONDOR_USER}"
+
+  id "${HTCONDOR_USER}"
+  if [ $? -ne 0 ]; then
+    useradd "${HTCONDOR_USER}"
+    if [ $? -ne 0 ]; then
+       echo "ERROR: Cannot crearte user ${HTCONDOR_USER}. Aborting."
+       exit 1
+    fi
+  fi
+  echo "HTCondor daemons launched as root but will run as user ${HTCONDOR_USER}"
+
+  (cd /opt/htcondor && ./condor_install --make-personal-condor --owner=${HTCONDOR_USER} --local-dir=/var/lib/htcondor )
+  if [ $? -ne 0 ]; then
+    echo "ERROR: condor_install failed. Aborting."
+    exit 1
+  fi
+
+  cp /opt/htcondor/condor.sh /etc/profile.d/
+
+else
+  echo "HTCondor daemons running as current user ${myuname}"
+
+  (cd /opt/htcondor && ./condor_install --make-personal-condor --local-dir=/var/lib/htcondor )
+  if [ $? -ne 0 ]; then
+    echo "ERROR: condor_install failed. Aborting."
+    exit 1
+  fi
+
+  echo "source /opt/htcondor/condor.sh" >> ~/.bashrc
+
 fi
 
 # The containers usaully have more CPUs visible than we can use
 # Limit to 1 CPU by default
-mkdir /var/lib/htcondor/config
 echo "NUM_CPUS=1" > /var/lib/htcondor/config/50_one_cpu.config
-
-
-if [ "${HTCONDOR_USER}" == "root" ]; then
-  cp /opt/htcondor/condor.sh /etc/profile.d/
-else
-  echo "source /opt/htcondor/condor.sh" >> ~/.bashrc
-fi
 
 echo "Starting HTCondor"
 
