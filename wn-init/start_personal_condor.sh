b24f0aba165ba4165e191965177c255978a61f57
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-06-14 11:41:56 +0200

    Support use as non-root user

--- wn-init/start_personal_condor.sh
+++ wn-init/start_personal_condor.sh
@@ -5,9 +5,16 @@
 # 
 #
 
+myuname=`id -un`
+
 if [ "${HTCONDOR_USER}" == "" ]; then
-  export HTCONDOR_USER=condor
+  if [ "${HTCONDOR_USER}" == "root" ]; then
+    export HTCONDOR_USER=condor
+  else
+    export HTCONDOR_USER=$myuname
+  fi
 fi
+echo "HTCondor daemons will run as user ${HTCONDOR_USER}"
 
 id "${HTCONDOR_USER}"
 if [ $? -ne 0 ]; then
@@ -26,6 +33,11 @@ if [ $rc -ne 0 ]; then
 fi
 
 mkdir -p /var/lib/htcondor/config
+rc=$?
+if [ $rc -ne 0 ]; then
+  echo "ERROR: Failed to create /var/lib/htcondor/config"
+  exit 1
+fi
 
 (cd /opt/htcondor && ./condor_install --make-personal-condor --owner=${HTCONDOR_USER} --local-dir=/var/lib/htcondor )
 if [ $? -ne 0 ]; then
@@ -39,7 +51,14 @@ mkdir /var/lib/htcondor/config
 echo "NUM_CPUS=1" > /var/lib/htcondor/config/50_one_cpu.config
 
 
-cp /opt/htcondor/condor.sh /etc/profile.d/
+if [ "${HTCONDOR_USER}" == "root" ]; then
+  cp /opt/htcondor/condor.sh /etc/profile.d/
+else
+  echo "source /opt/htcondor/condor.sh" >> ~/.bashrc
+fi
+
+echo "Starting HTCondor"
 
 source /opt/htcondor/condor.sh
 condor_master -f
+
