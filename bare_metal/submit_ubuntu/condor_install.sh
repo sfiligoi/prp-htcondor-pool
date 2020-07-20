cdc93512481fdc2859834b124744f00e57c2874f
Igor Sfiligoi <isfiligoi@ucsd.edu>
2020-07-20 16:01:34 -0700

    Add missing include

--- bare_metal/submit_ubuntu/condor_install.sh
+++ bare_metal/submit_ubuntu/condor_install.sh
@@ -12,6 +12,7 @@
 #  https://research.cs.wisc.edu/htcondor/instructions/ubuntu/18/stable/
 
 wget -qO - https://research.cs.wisc.edu/htcondor/ubuntu/HTCondor-Release.gpg.key | sudo apt-key add -
+echo "deb http://research.cs.wisc.edu/htcondor/ubuntu/8.8/bionic bionic contrib" >> /etc/apt/sources.list
 echo "deb-src http://research.cs.wisc.edu/htcondor/ubuntu/8.8/bionic bionic contrib" >> /etc/apt/sources.list
 apt-get update
 apt-get -y install htcondor
