f58044eea43623c81e8e668b4869e3415711d2f2
Igor Sfiligoi <isfiligoi@ucsd.edu>
2020-07-20 16:11:23 -0700

    Add eror checking

--- bare_metal/submit_ubuntu/condor_install.sh
+++ bare_metal/submit_ubuntu/condor_install.sh
@@ -12,16 +12,22 @@
 #  https://research.cs.wisc.edu/htcondor/instructions/ubuntu/18/stable/
 
 wget -qO - https://research.cs.wisc.edu/htcondor/ubuntu/HTCondor-Release.gpg.key | sudo apt-key add -
+if [ $? -ne 0 ]; then  echo "ERROR, aborting"; exit 1; fi
 echo "deb http://research.cs.wisc.edu/htcondor/ubuntu/8.8/bionic bionic contrib" >> /etc/apt/sources.list
+if [ $? -ne 0 ]; then  echo "ERROR, aborting"; exit 1; fi
 echo "deb-src http://research.cs.wisc.edu/htcondor/ubuntu/8.8/bionic bionic contrib" >> /etc/apt/sources.list
+if [ $? -ne 0 ]; then  echo "ERROR, aborting"; exit 1; fi
 apt-get update
+if [ $? -ne 0 ]; then  echo "ERROR, aborting"; exit 1; fi
 apt-get -y install htcondor
+if [ $? -ne 0 ]; then  echo "ERROR, aborting"; exit 1; fi
 
 #
 # Add configuration files
 #
 
 cp condor_config.d/* /etc/condor/config.d/
+if [ $? -ne 0 ]; then  echo "ERROR, aborting"; exit 1; fi
 
 #
 # Create a unique pool password
@@ -31,6 +37,7 @@ echo "=============================================="
 echo "Please provide a random HTCondor pool password 
 
 condor_store_cred -f /etc/condor/POOL
+if [ $? -ne 0 ]; then  echo "ERROR, aborting"; exit 1; fi
 
 #
 # We are ready to start condor
